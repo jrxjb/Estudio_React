34e2e298cf1c04e8453ace17655d5f2f48c9ae50
jrxjb <177959714+jrxjb@users.noreply.github.com>
2025-09-05 23:08:46 -0400

    update

--- FreeCodeCamp/aplicacion-de-tareas/aplicacion-react-tareas/src/App.js
+++ FreeCodeCamp/aplicacion-de-tareas/aplicacion-react-tareas/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import freeCodeCampLogo from './imagenes/FCC_LOGO.png';
-
+import Tarea from './componentes/tarea.js';
 function App() {
   return (
     <div className="aplicacion-tareas">
@@ -10,10 +10,11 @@ function App() {
           className="freecodecamp-logo" 
         />
       </div>
-
       <div className='tarea-lista-logo'>
         <h1>Mis tareas</h1>
+        <Tarea  texto='Aprender React'/>
       </div>
+
     </div>
   );
 }
--- FreeCodeCamp/aplicacion-de-tareas/aplicacion-react-tareas/src/index.js
+++ FreeCodeCamp/aplicacion-de-tareas/aplicacion-react-tareas/src/index.js
@@ -12,4 +12,4 @@ root.render(
 );
 
 
-// 5h 46m
+// 5h 56m 20s
